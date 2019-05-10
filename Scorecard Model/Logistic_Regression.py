18c992f624b628ae7ba9370130efe793540945fc
YGingko <ygingko@qq.com>
2019-05-10 18:36:27 +0800

    commit something

---
+++ Scorecard Model/Logistic_Regression.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 10 15:49:33 2019
+
+@author: Chocolate
+"""
+
+import numpy as np
+import pandas as pd
+
+train = pd.read_csv('new_train.csv', encoding='gbk')
+test = pd.read_csv('new_test.csv', encoding='gbk')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
